59e90a65efeba2207ccb148fb89e0a79d31dfd00
Joshua Crunden <joshuacrunden@gmail.com>
2021-02-26 17:41:48 +0000

    Write func for getting postcode and another for checking if postcode is in accepted list, write unit tests for checker func

--- lambda/src/main.go
+++ lambda/src/main.go
@@ -1,12 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 )
 
-func Handler() (string, error) {
-	return "Hello World", nil
+func checkIfPostcodeIsInSDC(postcode string, postcodes []string) bool {
+	result := false
+	for _, v := range postcodes {
+		if strings.HasPrefix(postcode, fmt.Sprintf("%s ", v)) {
+			result = true
+			break
+		}
+	}
+	return result
 }
+
+func getUserPostcode(deviceID string, accessToken string, apiEndpoint string, client *http.Client) (result string, err error) {
+	// /v1/devices/*deviceId*/settings/address/countryAndPostalCode
+	url := fmt.Sprintf("%s/v1/devices/%s/settings/address/countryAndPostalCode", apiEndpoint, deviceID)
+	req, err := http.NewRequest("GET", url, nil)
+	bearer := "Bearer " + accessToken
+	req.Header.Add("Authorization", bearer)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	fmt.Println(string(body))
+	result = "GL10 3GW"
+	return
+}
+func handler(request Request) (Response, error) {
+	//var response alexa.Response
+	postcodes := strings.Split(os.Getenv("SDC_POSTCODES"), " ")
+	fmt.Println(postcodes)
+	deviceID := request.Context.System.Device.DeviceID
+	accessToken := request.Context.System.APIAccessToken
+	apiEndpoint := request.Context.System.APIEndpoint
+	client := &http.Client{}
+	result, err := getUserPostcode(deviceID, accessToken, apiEndpoint, client)
+	if err != nil {
+		// do something with error
+	}
+	fmt.Println(result)
+
+	//switch request.Body.Intent.Name {
+	//	case "GetBinDayInfoIntent":
+	//   	response = HandleGetBinDayInfoIntent(request)
+	//}
+	fmt.Printf("Starting lambda\n")
+	return NewSimpleResponse("Saying Hello", "Hello world!"), nil
+}
+
 func main() {
-	lambda.Start(Handler)
+	lambda.Start(handler)
 }
--- lambda/src/main_test.go
+++ lambda/src/main_test.go
@@ -2,6 +2,17 @@ package main
 
 import "testing"
 
-func TestPlacehoder(t *testing.T) {
-
+func TestCheckIfPostcodeIsInSDCWhereItIs(t *testing.T) {
+	postcodes := []string{"SW1B", "SW1C", "SW1A", "SW1D"}
+	result := checkIfPostcodeIsInSDC("SW1A 2AA", postcodes)
+	if result != true {
+		t.Error("Expected true, got:", result)
+	}
+}
+func TestCheckIfPostcodeIsInSDCWhereItIsNot(t *testing.T) {
+	postcodes := []string{"SW1B", "SW1C", "SW1", "SW1D"}
+	result := checkIfPostcodeIsInSDC("SW1A 2AA", postcodes)
+	if result != false {
+		t.Error("Expected false, got:", result)
+	}
 }
